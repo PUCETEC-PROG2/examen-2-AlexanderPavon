bcdd4659f083ff36ca8ee5c89497253cf18483ee
PUCETEC <a.alexanderpavon@gmail.com>
2024-07-13 10:16:13 -0500

    Examen 2

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=30,null=False)
+    genre = models.CharField(max_length=30,null=False)
+    director = models.CharField(max_length=75,null=False)
+    release_year = models.IntegerField(null=False)
+    synopsis = models.TextField(null=False)
+    
+    def __str__(self) -> str:
+        return self.title
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+from .models import Movie
+
+def index(request):
+    movies = Movie.objects.order_by('release_year')
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({'movies': movies}, request))
+
+def movie(request, movie_id):
+    movie = Movie.objects.get(pk=movie_id)
+    template = loader.get_template('display_movie.html')
+    context = {
+        'movie': movie
+    }
+    return HttpResponse(template.render(context, request))
+
